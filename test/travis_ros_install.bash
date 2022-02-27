a0e84fefda372018ca89bada0c1a9633887f0a03
ken <ken@example.com>
2022-02-27 11:28:01 +0000

    modify travis_ros_install.bash

--- test/travis_ros_install.bash
+++ test/travis_ros_install.bash
@@ -5,24 +5,20 @@ pip install catkin_pkg
 pip install empy
 pip install pyyaml
 pip install rospkg
-echo $PYTHONPATH
 ls /home/travis/virtualenv/python2.7.14/lib/python2.7/site-packages
 
 #ros install
-echo $PYTHONPATH
 cd ..
 git clone https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu14.04_server.git
 cd ./ros_setup_scripts_Ubuntu14.04_server
 bash ./step0.bash
 bash ./step1.bash
-echo $PYTHONPATH
 
 #catkin setup
-echo $PYTHONPATH
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/src
 source /opt/ros/indigo/setup.bash
 catkin_init_workspace
 cd ~/catkin_ws
-echo $PYTHONPATH
+export PYTHONPATH=$PYTHONPATH:/home/travis/virtualenv/python2.7.14/lib/python2.7/site-packages
 catkin_make
