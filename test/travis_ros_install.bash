c94a302f3a499735668ce32f16a424e9af437ee2
ken <ken@example.com>
2022-02-27 11:20:46 +0000

    modify travis_ros_install.bash

--- test/travis_ros_install.bash
+++ test/travis_ros_install.bash
@@ -9,13 +9,16 @@ echo $PYTHONPATH
 ls /home/travis/virtualenv/python2.7.14/lib/python2.7/site-packages
 
 #ros install
+echo $PYTHONPATH
 cd ..
 git clone https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu14.04_server.git
 cd ./ros_setup_scripts_Ubuntu14.04_server
 bash ./step0.bash
 bash ./step1.bash
+echo $PYTHONPATH
 
 #catkin setup
+echo $PYTHONPATH
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/src
 source /opt/ros/indigo/setup.bash
