0d822e0c808ec5c9c68981782047953b61a75fb8
ken <ken@example.com>
2022-02-27 11:50:05 +0000

    modify travis_ros_install.bash

--- test/travis_package_make.bash
+++ test/travis_package_make.bash
@@ -3,6 +3,4 @@
 #sync and make
 rsync -av ./ ~/catkin_ws/src/pimouse_ros/
 cd ~/catkin_ws
-echo $PYTHONPATH
-export PYTHONPATH=$PYTHONPATH:/home/travis/virtualenv/python2.7.14/lib/python2.7/site-packages
 catkin_make
